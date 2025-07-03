52ccfda06536f38a11002431496308ee300c55ea
HreshchyshynT <hreshchyshyn.t@gmail.com>
2025-07-03 23:14:00 +0300

    add verbose flag

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 import os
 import sys
 import google.genai as genai
+from google.genai import types
 
 
 def main():
@@ -10,15 +11,22 @@ def main():
         print("Prompt is not provided")
         sys.exit(1)
     prompt = sys.argv[1]
+    verbose = "--verbose" in sys.argv
+
+    messages = [types.Content(role="user", parts=[types.Part(text=prompt)])]
+
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=prompt,
+        contents=messages,
     )
     prompt_tokens = response.usage_metadata.prompt_token_count
     response_tokens = response.usage_metadata.candidates_token_count
     print(response.text)
+    if not verbose:
+        return
+    print(f"User prompt: {prompt}")
     print(
         f"""Prompt tokens: {prompt_tokens}
 Response tokens: {response_tokens}"""
