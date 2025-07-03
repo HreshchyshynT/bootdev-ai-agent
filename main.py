6812b37f5dce55d543dc8e5384c1c8a96f4663fb
HreshchyshynT <hreshchyshyn.t@gmail.com>
2025-07-03 23:04:11 +0300

    add dynamic input

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from dotenv import load_dotenv
 import os
+import sys
 import google.genai as genai
 
 
 def main():
-    loaded = load_dotenv()
-    print(f"env loaded: {loaded}")
+    load_dotenv()
+    if len(sys.argv) < 2:
+        print("Prompt is not provided")
+        sys.exit(1)
+    prompt = sys.argv[1]
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+        contents=prompt,
     )
     prompt_tokens = response.usage_metadata.prompt_token_count
     response_tokens = response.usage_metadata.candidates_token_count
